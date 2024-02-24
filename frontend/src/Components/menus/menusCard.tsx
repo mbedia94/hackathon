b9829698002a0d1b48a03f634f166f0682f64165
Vincent <Vincent.rey865@icloud.com>
2024-02-24 13:46:52 +0100

    commit

--- frontend/src/Components/menus/menusCard.tsx
+++ frontend/src/Components/menus/menusCard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const MenusCard = ({ menu }) => (
   <div>
     <h2>{menu.name}</h2>
-    <p>{menu.plates}</p>
+    <p>{menu}</p>
     <p>{menu.prices}</p>
   </div>
 );
